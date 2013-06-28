040d5d6d60be8c1cf7dcaf13297a577318e9aff1
Hew Wolff <hew@platphormcorp.com>
2013-06-28 12:09:54 -0700

    do exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
+    'Ruby on Rails Tutorial Sample App' +
+     (!page_title.empty? ? " | #{page_title}" : '')
   end
 end

d71511de94bf99954b534f8d0fb72bc22e8381e8
Hew Wolff <hew@platphormcorp.com>
2013-07-08 15:57:34 -0700

    finish chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,6 +39,64 @@ describe "User pages" do
       it 'should create a user' do
         expect {click_button submit}.to change(User, :count).by(1)
       end
+
+      describe 'after saving the user' do
+        before {click_button submit}
+        let(:user) {User.find_by_email('user@example.com')}
+
+        it {should have_selector('title', text: user.name)}
+        it {should have_selector('div.alert.alert-success', text: 'Welcome')}
+      end
+    end
+
+    describe 'with invalid information' do
+      before do
+        fill_in 'Name', with: 'Example User'
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'foobar'
+        fill_in 'Confirmation', with: 'foobar'
+      end
+
+      describe 'in username' do
+        before do
+          fill_in 'Name', with: ''
+          click_button submit
+        end
+
+        it {should have_selector('title', text: 'Sign up')}
+        it {should have_content('error')}
+      end
+
+      describe 'in username' do
+        before do
+          fill_in 'Password', with: ''
+          click_button submit
+        end
+
+        it {should have_selector('title', text: 'Sign up')}
+        it {should have_content('error')}
+        it {should_not have_content('digest')}
+      end
+
+      describe 'in e-mail address' do
+        before do
+          fill_in 'Email', with: 'fred@hotmail.'
+          click_button submit
+        end
+
+        it {should have_selector('title', text: 'Sign up')}
+        it {should have_content('error')}
+      end
+
+      describe 'in confirmation' do
+        before do
+          fill_in 'Confirmation', with: 'xxx'
+          click_button submit
+        end
+
+        it {should have_selector('title', text: 'Sign up')}
+        it {should have_content('error')}
+      end
     end
   end
 end

e039929e572b59423f400980849f43daa12477cd
Hew Wolff <hew@platphormcorp.com>
2013-07-13 08:13:03 -0700

    add authentication to user forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -10,7 +11,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -25,7 +25,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = 'Profile updated'
       sign_in @user
@@ -39,5 +38,10 @@ class UsersController < ApplicationController
 
   def signed_in_user
     redirect_to signin_url, notice: 'Please sign in.' unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end

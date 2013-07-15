45db211fb2f3558901ee0b79bbdb71724cab1015
Hew Wolff <hew@platphormcorp.com>
2013-07-15 15:39:56 -0700

    finish 9.6.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :non_signed_in_user, only: [:new, :create]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
 
@@ -51,6 +52,12 @@ class UsersController < ApplicationController
     unless signed_in?
       store_location
       redirect_to signin_url, notice: 'Please sign in.'
+    end
+  end
+
+  def non_signed_in_user
+    if signed_in?
+      redirect_to root_path
     end
   end
 

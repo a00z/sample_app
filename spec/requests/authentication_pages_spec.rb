83683ff0819a1faadc551b902510d212e4d8eb6e
Hew Wolff <hew@platphormcorp.com>
2013-07-15 14:46:48 -0700

    finish 9.6.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -106,5 +106,19 @@ describe "Authentication" do
         specify {response.should redirect_to(root_path)}
       end
     end
+
+    describe 'accessible attributes' do
+      it 'should not allow access to admin' do
+        expect do
+          User.new(
+            name: 'person',
+            email: 'person@example.com',
+            password: 'foobar',
+            password_confirmation: 'foobar',
+            admin: true
+          )
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+    end    
   end
 end

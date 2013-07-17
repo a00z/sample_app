59c9060bbf7f5b84ca81161d342c89794358d0c1
Hew Wolff <hew@platphormcorp.com>
2013-07-17 10:29:31 -0700

    finish 10.1.3

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Micropost do
+  let(:user) {FactoryGirl.create(:user)}
+  before do
+    @micropost = user.microposts.build(content: 'Lorem ipsum')
+  end
+
+  subject {@micropost}
+
+  it {should respond_to(:content)}
+  it {should respond_to(:user_id)}
+
+  it {should be_valid}
+
+  describe 'when user is not present' do
+    before {@micropost.user_id = nil}
+    it {should_not be_valid}
+  end
+
+  describe 'accessible attributes' do
+    it 'should not allow access to user_id' do
+      expect do
+        Micropost.new(user_id: user.id)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+end
